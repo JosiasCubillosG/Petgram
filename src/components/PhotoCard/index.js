6e5fa39977302aa3b45eabf0f2332d52f1571475
MI PC <josiascubillosg@gmail.com>
2020-02-10 12:39:24 -0500

    I implemented lazy loader with custom hook

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,19 +1,41 @@
-import React from 'react'
+import React, {Fragment,useEffect,useRef,useState} from 'react'
 import {ImgWrapper, Button, Img} from './styles'
 import {MdFavoriteBorder} from 'react-icons/md'
+import {Article} from './styles'
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
 export const PhotoCard = ({id, likes = 0, src = DEFAULT_IMAGE}) => {
+    const element = useRef(null)
+    const [show,setShow] = useState(false)
+
+    // Hook para crear un lazy loader
+    useEffect(function(){
+        const observer = new window.IntersectionObserver(function(entries){
+            const {isIntersecting} = entries[0]
+            console.log(isIntersecting)
+            if(isIntersecting){
+                setShow(true)
+                observer.disconnect()
+            }
+        })
+        observer.observe(element.current)
+    },[element])
+
     return(
-        <article>
-            <a href={`/detail/${id}`}>
-                <ImgWrapper>
-                    <Img src={src} />
-                </ImgWrapper>
-            </a>
-            <Button>
-                <MdFavoriteBorder size={'32px'} />{likes} likes!
-            </Button>
-        </article>
+        <Article ref={element}>
+            {
+                show &&
+                <Fragment>
+                    <a href={`/detail/${id}`}>
+                        <ImgWrapper>
+                            <Img src={src} />
+                        </ImgWrapper>
+                    </a>
+                    <Button>
+                        <MdFavoriteBorder size={'32px'} />{likes} likes!
+                    </Button>
+                </Fragment>
+            }
+        </Article>
     )
 }
